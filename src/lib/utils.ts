7cfed374051f1d6c60d10067dbcb0b66dd8769ba
Thong Pham <>
2026-03-04 14:07:56 +0700

    feat: add formatDate utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(date: string | Date | number, locale = 'en-US') {
+  return new Intl.DateTimeFormat(locale, {
+    dateStyle: 'medium',
+    timeStyle: 'short',
+  }).format(new Date(date))
+}
